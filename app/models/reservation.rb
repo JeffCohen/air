389288e460094a945b2440ab227245b2a7775d7d
Arjun Venkataswamy <avenkat2@gmail.com>
2013-05-24 09:14:22 -0500

    adds credit card validation to reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,11 @@
 class Reservation < ActiveRecord::Base
   attr_accessible :credit_card_num, :departure_date, :flight_id, :user_id
 
+  validate :valid_credit_card_num
 
+  def valid_credit_card_num
+    unless credit_card_num.starts_with?('4') && credit_card_num.length == 16
+      errors.add(:credit_card, 'must be Visa')
+    end
+  end
 end
