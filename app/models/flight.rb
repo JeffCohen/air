f3a9f9757354a2a9b6f0b03b58b6df5ea355aa94
Arjun Venkataswamy <avenkat2@gmail.com>
2013-05-24 11:32:29 -0500

    adds reservations to user profile and refactors code into a reservation method

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,6 +4,7 @@ class Flight < ActiveRecord::Base
   belongs_to :departure_airport, class_name: 'Airport', foreign_key: 'departure_airport_id'
   belongs_to :arrival_airport, class_name: 'Airport', foreign_key: 'arrival_airport_id'
 
+  has_many :reservations
 
   MILEAGE_HASH = {  'ORD - JFK' => 740,
                     'ORD - LAX' => 1744,
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,9 @@
 class Reservation < ActiveRecord::Base
   attr_accessible :credit_card_num, :departure_date, :flight_id, :user_id
 
+  belongs_to :user
+  belongs_to :flight
+
   validate :valid_credit_card_num
 
   def valid_credit_card_num
@@ -8,4 +11,21 @@ class Reservation < ActiveRecord::Base
       errors.add(:credit_card, 'must be Visa')
     end
   end
+
+  def formatted
+    reservation = ""
+    reservation << "Flight ##{self.flight.number}. "
+    reservation << self.departure_date.strftime('%m/%e/%y')
+    flight = self.flight
+    reservation <<  " #{flight.departure_airport.code} - "
+    reservation << flight.arrival_airport.code
+    reservation << " #{flight.departs_at.strftime('%l:%M%p')} -"
+    reservation << flight.arrives_at.strftime('%l:%M%p')
+    return reservation
+  end
 end
+
+
+
+
+
